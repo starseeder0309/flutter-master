2f8dd15bf0da90d1e523d5b4f1501ab752a5db1c
kimhyeonu <polaris@byeolmuri.com>
2021-10-16 23:41:33 +0900

    3.2 플러터 위젯 사용법

--- do-it-flutter/C01/first_flutter_app/lib/main.dart
+++ do-it-flutter/C01/first_flutter_app/lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    print('createState()');
     return _MyApp();
   }
 }
@@ -18,6 +19,7 @@ class _MyApp extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    print('build()');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -48,5 +50,17 @@ class _MyApp extends State<MyApp> {
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState()');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('didChangeDependencies()');
   }
 }

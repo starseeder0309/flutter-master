ad8ac6f47d55fa68aef0cb2f8c296dd718814404
kimhyeonu <polaris@byeolmuri.com>
2022-02-12 18:06:30 +0900

    프로젝트 정리

--- do-it-flutter/C02/Code02_09.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:async';
-
-main() async {
-  var stream = Stream.fromIterable([1, 2, 3, 4, 5]);
-
-  stream.last.then((value) => print('last: $value'));
-}
--- do-it-flutter/C02/Program02_01.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  for (int i = 0; i <= 9; i++) {
-    for (int j = 1; j <= 9; j++) {
-      print('$i * $j = ${i * j}');
-    }
-  }
-}
